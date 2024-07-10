e7e354e7e9543879151dfde0f70b5f7ac32c8c34
g2patil <patilg2@yahoo.co.in>
2024-07-10 22:58:26 +0530

    20240710_01

--- HomeScreen.js
+++ HomeScreen.js
@@ -4,24 +4,24 @@ import { StyleSheet, Text, View, TouchableOpacity, Image, Alert, FlatList } from
 
 export default HomeScreen = () => {
   options = [
-    { id: 1, title: 'Option 1', image: 'https://img.icons8.com/color/70/000000/cottage.png' },
+    { id: 1, title: 'Add Renter', image: 'https://img.icons8.com/color/70/000000/cottage.png' },
     {
       id: 1,
-      title: 'Option 2',
+      title: 'Update Rentor',
       image: 'https://img.icons8.com/color/70/000000/administrator-male.png',
     },
-    { id: 2, title: 'Option 3', image: 'https://img.icons8.com/color/70/000000/filled-like.png' },
-    { id: 3, title: 'Option 4', image: 'https://img.icons8.com/color/70/000000/facebook-like.png' },
-    { id: 4, title: 'Option 5', image: 'https://img.icons8.com/color/70/000000/shutdown.png' },
-    { id: 5, title: 'Option 6', image: 'https://img.icons8.com/color/70/000000/traffic-jam.png' },
-    {
+    { id: 2, title: 'update rent', image: 'https://img.icons8.com/color/70/000000/filled-like.png' },
+    { id: 3, title: 'modify Rent', image: 'https://img.icons8.com/color/70/000000/facebook-like.png' },
+    { id: 4, title: 'View Rent', image: 'https://img.icons8.com/color/70/000000/shutdown.png' },
+    { id: 5, title: 'View Pending Rent', image: 'https://img.icons8.com/color/70/000000/traffic-jam.png' },
+  /*  {
       id: 6,
       title: 'Option 7',
       image: 'https://img.icons8.com/dusk/70/000000/visual-game-boy.png',
     },
     { id: 8, title: 'Option 8', image: 'https://img.icons8.com/flat_round/70/000000/cow.png' },
     { id: 9, title: 'Option 9', image: 'https://img.icons8.com/color/70/000000/coworking.png' },
-    { id: 9, title: 'Option 10', image: 'https://img.icons8.com/nolan/70/000000/job.png' },
+    { id: 9, title: 'Option 10', image: 'https://img.icons8.com/nolan/70/000000/job.png' },*/
   ]
 
   constclickEventListener = () => {
